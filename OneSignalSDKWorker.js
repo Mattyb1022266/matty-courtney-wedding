cb311c80edf947cbef27ed10ca6c2474e6a53f1a
Mattyb1022266 <matty@comparethenetworks.com>
2026-04-12 23:25:58 +0100

    Delete OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++
@@ -1,3 +0,0 @@
-// OneSignal Service Worker — required for push notifications
-// This file must be at the ROOT of your site (same folder as index.html)
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
